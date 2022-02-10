8df5e7b820af8b4415841fb0e237dd051533692e
Luís Eduardo Bernardino Luz <dudubluz@gmail.com>
2022-02-10 14:35:10 -0300

    Add project files.

---
+++ Tupiniquim.ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Tupiniquim.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string posicaoInicial;
+
+            posicaoInicial = "0,0,N";
+
+            Console.WriteLine("Robô Tupiniquim em missão à Marte!");
+            Console.Write("\nSeu posicionamento é: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(posicaoInicial);
+            Console.ResetColor();
+            Console.Write("Como deseja se mover? Digite: ");
+
+            Console.ReadLine();
+        }
+    }
+}
