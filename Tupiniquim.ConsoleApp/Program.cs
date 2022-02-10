cf4ee344f4bd2865fdf7dade401cb631c76150b9
Luís Eduardo Bernardino Luz <dudubluz@gmail.com>
2022-02-10 16:29:17 -0300

    Robô Tupiniquim desafio Academia do Programador

--- Tupiniquim.ConsoleApp/Program.cs
+++ Tupiniquim.ConsoleApp/Program.cs
@@ -6,18 +6,95 @@ namespace Tupiniquim.ConsoleApp
     {
         static void Main(string[] args)
         {
-            string posicaoInicial;
+            string comando, posicaoInicial;
+            int posY = 0;
+            int posX = 0; 
+            char direcao = 'N';            
 
-            posicaoInicial = "0,0,N";
-
-            Console.WriteLine("Robô Tupiniquim em missão à Marte!");
-            Console.Write("\nSeu posicionamento é: ");
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine(posicaoInicial);
+            Console.WriteLine("Robô Tupiniquim em missão à Marte!\n");                        
             Console.ResetColor();
-            Console.Write("Como deseja se mover? Digite: ");
+                        
+            Console.Write("\nDigite o a altura onde está o Robô: ");
+            posY = Convert.ToInt32(Console.ReadLine());
 
-            Console.ReadLine();
+            Console.Write("Agora, a base onde está o Robô: ");
+            posX = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Em seguida, digite a rotação inicial: ");
+            posicaoInicial = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Digite os comandos de movimento: ");
+            comando = Console.ReadLine();
+
+            char[] instrucoes = comando.ToCharArray();
+
+            for (int i = 0; i < instrucoes.Length; i++)
+            {
+                if (instrucoes[i] == 'M')
+                {
+                    if (direcao == 'N')
+                    {
+                        posY = posY + 1;
+                    }
+                    if (direcao == 'S')
+                    {
+                        posY = posY - 1;
+                    }
+                    if (direcao == 'L')
+                    {
+                        posX = posX + 1;
+                    }
+                    if (direcao == 'O')
+                    {
+                        posX = posX - 1;
+                    }
+                }
+                if (instrucoes[i] == 'D')
+                {
+                    if (direcao == 'N')
+                    {
+                        direcao = 'L';
+                    }
+                    else if (direcao == 'L')
+                    {
+                        direcao = 'S';
+                    }
+                    else if (direcao == 'S')
+                    {
+                        direcao = 'O';
+                    }
+                    else if (direcao == 'O')
+                    {
+                        direcao = 'N';
+                    }
+                }
+                if (instrucoes[i] == 'E')
+                {
+                    if (direcao == 'N')
+                    {
+                        direcao = 'O';
+                    }
+                    else if (direcao == 'O')
+                    {
+                        direcao = 'S';
+                    }
+                    else if (direcao == 'S')
+                    {
+                        direcao = 'L';
+                    }
+                    else if (direcao == 'L')
+                    {
+                        direcao = 'N';
+                    }
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Sua posição final é: " + posX + " " + posY + " " + direcao + ".");
+            Console.ResetColor();
+
+               Console.ReadLine();
         }
     }
 }
